44b082a47c7440b2c74b629ae4e5c85d19a3b036
Arttu <arttu.lankinen@edu.savonia.fi>
2024-05-21 12:39:39 +0300

    login function

--- src/Sivut/LogIn.js
+++ src/Sivut/LogIn.js
@@ -1,23 +1,56 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 function LogIn() {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+    const response = await fetch('/api/auth/login', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ username, password })
+    });
+    const data = await response.json();
+    if (response.ok) {
+      localStorage.setItem('token', data.token);
+      alert('Login successful');
+    } else {
+      alert(data.msg);
+    }
+  };
+
   return (
-      <div id='loginsivu'>
-        <form id='loginData'>
-          <div className='loginBG'>
-            <label>Käyttäjänimi:</label>
-            <input type='text' placeholder='MaijaMeikälainen' className='login'/>
-          </div>
-          <div className='loginBG'>
-            <label>Salasana:</label>
-            <input type='password' placeholder='********' className='login'/>
-          </div>
-          <div>
-              <button id='sendL' type='submit'>Kirjaudu</button>
-          </div>
-        </form>
-      </div>
-  )
+    <div id='loginsivu'>
+      <form id='loginData' onSubmit={handleLogin}>
+        <div className='loginBG'>
+          <label>Käyttäjänimi:</label>
+          <input 
+            type='text' 
+            placeholder='MaijaMeikälainen' 
+            className='login' 
+            value={username} 
+            onChange={(e) => setUsername(e.target.value)} 
+          />
+        </div>
+        <div className='loginBG'>
+          <label>Salasana:</label>
+          <input 
+            type='password' 
+            placeholder='********' 
+            className='login' 
+            value={password} 
+            onChange={(e) => setPassword(e.target.value)} 
+          />
+        </div>
+        <div>
+          <button id='sendL' type='submit'>Kirjaudu</button>
+        </div>
+      </form>
+    </div>
+  );
 }
 
-export {LogIn}
+export { LogIn };
