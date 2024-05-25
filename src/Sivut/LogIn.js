e4de053801e51bfb1eea56bc4c03548348ca3060
Arttu Lankinen <155956454+arttulankinen@users.noreply.github.com>
2024-05-25 13:21:26 +0300

    Update LogIn.js

--- src/Sivut/LogIn.js
+++ src/Sivut/LogIn.js
@@ -1,7 +1,7 @@
 
 import React, {useState} from 'react'
 import { useNavigate } from 'react-router-dom';
-import config from './config';
+
 
 function LogIn() {
   const [username, setUsername] = useState('');
@@ -10,7 +10,7 @@ function LogIn() {
 
   const handleLogin = async (e) => {
     e.preventDefault();
-    const response = await fetch(`${backendBaseUrl}/api/auth/login`, {
+    const response = await fetch('/api/auth/login', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json'
