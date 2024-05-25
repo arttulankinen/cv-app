4b3b47c79f4e8dc10c761e9d119018347c3790a6
Arttu Lankinen <155956454+arttulankinen@users.noreply.github.com>
2024-05-25 13:17:11 +0300

    Update Register.js

--- src/Sivut/Register.js
+++ src/Sivut/Register.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import config from './config.js
+import config from './config.js';
 
 function Register() {
   const navigate = useNavigate();
