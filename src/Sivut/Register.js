9232bb95f858f51148dbade1e728d5ab0a07cf6c
Arttu Lankinen <155956454+arttulankinen@users.noreply.github.com>
2024-05-25 13:02:12 +0300

    Update Register.js

--- src/Sivut/Register.js
+++ src/Sivut/Register.js
@@ -10,7 +10,7 @@ function Register() {
 
   const handleRegister = async (e) => {
     e.preventDefault();
-    const response = await fetch('http://localhost:5000/api/auth/register', {
+    const response = await fetch('/api/auth/register', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
